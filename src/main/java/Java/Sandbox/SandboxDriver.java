1bc0edaa0003f95a356c8d0321dba7585b92a5f2
Justin Dowell <Hashtag@Mac.local>
2019-11-22 19:57:54 -0500

    Initial Commit / Push

---
+++ src/main/java/Java/Sandbox/SandboxDriver.java
@@ -0,0 +1,13 @@
+package Java.Sandbox;
+
+public class SandboxDriver {
+	
+	/**
+	 * This is the main driving method.
+	 * 		- args is unhandled at this moment.
+	 * @param args Standard command-line arguments.
+	 */
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }// ==============================
+}// ==============================
